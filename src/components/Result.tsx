8a5ea5c188a610da6a13658e7adbbc812dfc8620
AhsanT4riq <ahsan_work@outlook.com>
2025-07-30 16:47:42 +1000

    fix: fix result screen

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -31,12 +31,9 @@ const Result = ({ result }: ResultProps) => {
       </Animated.Text>
       <Animated.Text entering={SlideInRight.delay(300)} className="text-xl font-bold text-gray-800">
         Term: <Text className="text-orange-700">{result.term + ' years'}</Text>
-        <Animated.Text
-          entering={SlideInRight.delay(500)}
-          className="text-xl font-bold text-gray-800"
-        >
-          Frequency: <Text className="text-orange-700">{result.frequency}</Text>
-        </Animated.Text>
+      </Animated.Text>
+      <Animated.Text entering={SlideInRight.delay(500)} className="text-xl font-bold text-gray-800">
+        Frequency: <Text className="text-orange-700">{result.frequency}</Text>
       </Animated.Text>
       <Animated.Text entering={SlideInRight.delay(400)} className="text-xl font-bold text-gray-800">
         Final Balance: <Text className="text-orange-700">{result.formattedFinalBalance}</Text>
